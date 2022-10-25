1fd21ca96602def775710f034a408719ce581103
Anton Konovalov <antoniuskonovalov@gmail.com>
2022-10-25 20:01:32 +0300

    NavLink

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar';
 import Explore from './pages/Explore';
 import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
@@ -8,16 +9,17 @@ import SignUp from './pages/SignUp';
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Routes>
-        <Route path='/' element={<Explore />} />
+        <Route path='/explore' element={<Explore />} />
         <Route path='/offers' element={<Offers />} />
         <Route path='/profile' element={<SignIn />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
-      </Routes>
-    </BrowserRouter>
+      </Routes> 
+      <Navbar />
+    </>
   );
 }
 
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import { NavLink, useLocation } from 'react-router-dom'
+import { useState } from 'react'
+//@ts-ignore
+import { ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg'
+//@ts-ignore
+import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg'
+//@ts-ignore
+import { ReactComponent as PersonOutlineIcon } from '../assets/svg/personOutlineIcon.svg'
+
 
 const Navbar = () => {
+  const [active, setActive] = useState<boolean>(false)
+  const location = useLocation()
+  
   return (
-    <div>
-      Navbar
-    </div>
+    <footer className='navbar'>
+      <nav className='navbarNav'>
+        <ul className= 'navbarListItems'>
+          <li className="navbarListItem">
+
+            <NavLink 
+              to='/explore'>  
+              <ExploreIcon className='footerIcon' fill={location.state}  />
+              <p>Explore</p>
+            </NavLink>
+          </li>
+          <li className="navbarListItem">
+
+            <NavLink 
+              to='/offers'>
+                <OfferIcon className='footerIcon'  fill={location.state} />
+                <p>Offer</p> 
+            </NavLink>
+          </li>
+          <li className="navbarListItem">
+            
+            <NavLink 
+              to='/profile'>
+                <PersonOutlineIcon className='footerIcon' fill={location.state} />
+                <p>Profile</p>
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </footer>
   )
 }
 
